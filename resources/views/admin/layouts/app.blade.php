b0b21163dc27d5cbec9d2ac17c07d55221a11da9
mumo-dev <samumo.dev@gmail.com>
2019-07-03 08:13:13 +0300

    hey there we come

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -67,7 +67,7 @@
     <script src="{{ asset('js/feather.min.js') }}"></script>
 
     <script src="{{ asset('/js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('/js/dataTables.bootstrap4.min.js') }}"></script>
 
     <script>
       feather.replace()
