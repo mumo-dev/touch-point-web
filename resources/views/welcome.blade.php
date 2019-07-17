1c36c0c21db0ae4f40e8494cd1c6c7edf8724ab2
mumo-dev <samumo.dev@gmail.com>
2019-07-17 21:31:24 +0300

    final commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,7 +88,7 @@
 
                 <div class="content">
                 <div class="title m-b-md">
-                    Touch
+                    Touch Rahisi
                     <img src="/images/logo.jpg" alt="LOGO">
                 </div>
 
